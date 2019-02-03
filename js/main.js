707d02f29067e976d02c7410fb41c5de9a093fbc
Martin Alpine <gorskigoral@gmail.com>
2019-02-03 11:15:18 +0100

    Localstorage implemented + color styles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,48 @@
 (function() {
   let blocks = document.querySelectorAll(".grid__block");
+  let tilemap = {};
+  tilemap.array = Array(9)
+    .fill(0)
+    .map(x => Array(12).fill(0));
+
+  tilemap.assignTiles = function(blocks) {
+    for ([index, block] of blocks.entries()) {
+      // console.log(parseInt(index / 12));
+      // console.log(parseInt(index % 12));
+      // console.log("----" + index + "----");
+      if (this.array[parseInt(index / 12)][parseInt(index % 12)] == 1)
+        block.classList.toggle("unlocked");
+    }
+  };
+  // console.dir(tilemap);
+
+  if (localStorage.tilemap) {
+    // const array2DMap =
+    // const arrayMap = stringMap.split(",");
+    // console.log(stringMap.length);
+    // console.log(stringMap);
+    // arrayMap.reduce(function(prev, curr, index, arr) {
+    //   console.log(prev + " " + curr + " " + index);
+    //   if (prev == 11) {
+    //     array2DMap.push(arr.slice(index, index + 12));
+    //     console.dir(array2DMap);
+    //     return 0;
+    //   } else {
+    //     return ++prev;
+    //   }
+    // }, 0);
+
+    tilemap.array = JSON.parse(localStorage.getItem("tilemap"));
+    tilemap.assignTiles(blocks);
+  }
+
   for (block of blocks) {
     block.addEventListener(
       "click",
-      function() {
-        this.classList.toggle("unlocked");
+      e => {
+        e.target.classList.toggle("unlocked");
+        tilemap.array[e.target.offsetTop / 64][e.target.offsetLeft / 64] = 1;
+        localStorage.setItem("tilemap", JSON.stringify(tilemap.array));
       },
       true
     );
